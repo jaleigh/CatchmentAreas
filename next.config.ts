a6c36fa42649222f715da6e59eef0a8810e1add1
James <james_a_leigh@yahoo.co.uk>
2025-01-21 22:31:28 +0000

    update config for deploying to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
@@ -8,6 +9,13 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/CatchmentAreas/' : '',
+  basePath: isProd ? '/CatchmentAreas' : '',
+  output: 'export'
 };
 
 export default nextConfig;
